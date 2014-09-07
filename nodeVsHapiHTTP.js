b0abd63c77a13254e15f409684a77337efef46cf
MMendes999 <mario.c.mendes1985@gmail.com>
2014-09-07 21:29:12 +0100

    implements pure node approach

--- nodeVsHapiHTTP.js
+++ nodeVsHapiHTTP.js
@@ -9,17 +9,33 @@ allow us to compare simplicity/complexity of the code.
 
 var http = require('http');
 var t = require('./modules/myModule.js');
-var url = require('url');
+
 
 var server = http.createServer();
 
-server.on('request', function(request, response){
-  console.log("Request received!");
-  response.writeHead(200, {"Content-type": "text/plain"});
-  response.end();
-});
+
+server.on("request", function(request, response){
+    console.log("Request received!");
+    console.log(request.url + " ------- " + typeof(request.url));
+    var myPath = request.url.split('/');
+    console.log(myPath);
+    console.log(myPath[1] + "------" + myPath[2]);
+
+      if(myPath[1] === 'circ'){
+        response.writeHead (200, {"Content-type": "text/plain"});
+        response.write("The value of the circumfrence is " + t.circumference(myPath[2]));
+      }
+      if(myPath[1] === 'area'){
+        response.writeHead (200, {"Content-type": "text/plain"});
+        response.write("The value of the area is " + t.area(myPath[2]));
+      }
+
+    response.end();
+  }
+);
 
 server.listen(8080);
+console.log("Server running on port 8080");
 
 
 
@@ -55,3 +71,12 @@ server.route(routes);
 server.start(function(){
   console.log("Server info " + server.info.uri);
 });
+
+
+/*
+Although the amount of code is similar, the simplicity of logic behind hapi is greater. When using the pure node approach
+I had to parse the request url myself and split it into an array and then set conditions. This would become cumbersome for a
+full blown project with a few hundred urls. I was giving an extra solution to the pure node approach, but again this is limited to
+a single url. If you are interested go to: http://stackoverflow.com/questions/25711324/setting-routes-in-node/25711860?noredirect=1#comment40197195_25711860
+I am sure there are other ways to do it, but this was the simplest way I found at the time, and without copying any solutions.
+*/
